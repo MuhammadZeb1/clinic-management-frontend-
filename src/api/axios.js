316f2b3901434d9d1a0ef8ba53462cd75107b568
MuhammadZeb1 <zebafridiuu@gmail.com>
2026-05-18 16:36:47 +0500

    add the link of the versal

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://clinic-management-backend-snyj.vercel.app/api",
+  // baseURL: "http://localhost:4000/api",
 });
 
 // Attach token automatically
